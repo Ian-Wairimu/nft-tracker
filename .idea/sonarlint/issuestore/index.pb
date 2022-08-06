4fa185d21798ed14a99ae430e34b24d797ce995e
ian wairimu <wairimuianmoon@gmail.com>
2022-08-06 12:53:56 +0300

    Implemented the Footer Component and made it responsive

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+A
+public/index.html,f/4/f43ecdf8d666ee908aacce037b4cd72159e4741b
+F
+src/components/App.jsx,c/5/c535b7d62672a2033395b4988bf9a1e136af2a71
+N
+src/components/images/logo.svg,b/5/b528047eaab4d7c8a4ab8eebcc059ee03518f4c3
+=
+src/index.css,c/5/c5d38a62d1faae90a2acaa34b79a7ef365c917e4
+<
+src/index.js,f/9/f900edc44cb929c2267a16100a32851f6d5b477d
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import "./footer.css";
+import {Link} from "react-router-dom";
+import {FaFacebookF, FaInstagram, FaTwitter} from "react-icons/fa";
+
+const Footer = () => {
+    return (
+        <>
+            <footer className="ft-con-main">
+                <section className="ft-sec">
+                    <h2 className="ft-sec-h1">QUICK LINKS</h2>
+                    <nav className="ft-nav">
+                        <ul className="ft-nav-items">
+                            <li className="ft-nav-lists">
+                                <Link to="/dashboard" className="ft-nav-links">
+                                    Dashboard
+                                </Link>
+                            </li>
+                            <li className="ft-nav-lists">
+                                <Link to="/market" className="ft-nav-links">
+                                    Market prices
+                                </Link>
+                            </li>
+                            <li className="ft-nav-lists">
+                                <Link to="/nft" className="ft-nav-links">
+                                    NFTs
+                                </Link>
+                            </li>
+                            <li className="ft-nav-lists">
+                                <Link to="/overview" className="ft-nav-links">
+                                    Overview
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </section>
+                <section className="ft-sec">
+                    <h2 className="ft-sec-h1">QUICK LINKS</h2>
+                    <nav className="ft-nav">
+                        <ul className="ft-nav-items">
+                            <li className="ft-nav-lists">
+                                <Link to="/profile" className="ft-nav-links">
+                                    My profile
+                                </Link>
+                            </li>
+                            <li className="ft-nav-lists">
+                                <Link to="/notifications" className="ft-nav-links">
+                                    Notifications
+                                </Link>
+                            </li>
+                            <li className="ft-nav-lists">
+                                <Link to="/settings" className="ft-nav-links">
+                                    Settings
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </section>
+                <section className="ft-sec">
+                    <h2 className="ft-sec-h1">RESOURCES</h2>
+                    <nav className="ft-nav">
+                        <ul className="ft-nav-items">
+                            <li className="ft-nav-lists">
+                                <Link to="/connect-wallet" className="ft-nav-links">
+                                    Connect wallet
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </section>
+                <section className="ft-sec tracker">
+                    {/*<span className="ft-logo"></span>*/}
+                    <h2 className="ft-sec-h1">Tracker</h2>
+                    <p className="ft-p">TrustTracker provides a fundamental analysis of the crypto market. In addition to tracking prices, volume and market capitalization</p>
+                    <div className="ft-brand-logo">
+                        <span className="ft-icon-con">
+                            <FaFacebookF />
+                        </span>
+                        <span className="ft-icon-con">
+                            <FaTwitter />
+                        </span>
+                        <span className="ft-icon-con">
+                            <FaInstagram />
+                        </span>
+                    </div>
+                </section>
+            </footer>
+        </>
+    )
+}
+
+export default Footer;
