2d8d8ee53d5dbf24e6ccc74f933c7890910b0f8a
unknown <dirambora>
2022-09-09 21:51:54 +0300

    Add History to App.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import Footer from "./footer/Footer";
 import Navigation from "./Navigation/Navigation";
 import Intro from "./Intro/Intro";
+import History from "./History/History";
 
 const App = () => {
     return (
-        <>
+        <div className="App">
             <Navigation/>
             <Intro/>
-            <Footer />
+            <History/>
            
-        </>
+        </div>
     )
 }
 
